2bb17ebbda1af8b0bc4dfee5327fd425484fb20d
gitchrisqueen <christopher.queen@gmail.com>
2024-02-21 17:17:45 -0500

    Added cron action and py file

--- export_requirements.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-#
-# Copyright (c) 2024. Christopher Queen Consulting LLC (http://www.ChristopherQueenConsulting.com/)
-#
-
-#############################################
-
-poetry export --without-hashes --format=requirements.txt > requirements.txt
