67001cd80bc9367140f19a7aa7d7d637bc2a4b8f
gitchrisqueen <christopher.queen@gmail.com>
2024-02-21 14:16:02 -0500

    Updates to include selenium driver and utils

--- src/cqc_smm/utilities/selenium_util.py
+++ src/cqc_smm/utilities/selenium_util.py
@@ -11,6 +11,16 @@ from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.service import Service
+import chromedriver_autoinstaller
+from pyvirtualdisplay import Display
+display = Display(visible=False, size=(800, 800))
+display.start()
+
+chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
+                                      # and if it doesn't exist, download it automatically,
+                                      # then add chromedriver to path
+
 
 from cqc_smm.utilities.env_constants import *
 from cqc_smm.utilities.logger import logger
